e6f2a4b8b3db9a776ec2f2bb2faf68cdff9f5069
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-10-29 23:53:30 +0300

    flow

--- App.jsx
+++ App.jsx
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context'; // Eklendi
+import HomeScreen from './src/Screens/HomeScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
+};
+
+export default App;
